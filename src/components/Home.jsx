1babaa3ba2de0db887b4bb06ea97a567b7a63ca5
jlisham <jlisham@hotmail.com>
2020-09-01 12:07:58 -0500

    updated index

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,6 @@ const Home = () => {
     <div className="row">
       <div className="col-md-4 col-xs-12 text-center"></div>
       <div className="col-md-4 col-xs-12">
-        <img
-          src="/images/vinod.jpg"
-          alt="Vinod Kumar"
-          className="img img-thumbnail"
-        />
         <h3>Movie Browser App</h3>
         <p>Pwrd by Reactjs </p>
       </div>
