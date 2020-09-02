5212449038ec45b22560fe0fd678f48d5fdfed9a
jlisham <jlisham@hotmail.com>
2020-09-01 22:15:37 -0500

    updated index

---
+++ build/precache-manifest.b808aad3d8d175889a98c06c1a8d47c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcb54579b71f82b72f4f90ff4b10596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b3425ef8549a8dcef1",
+    "url": "/static/js/2.1396ea37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1396ea37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435416c3db33a4080e3a",
+    "url": "/static/js/main.2a727272.chunk.js"
+  },
+  {
+    "revision": "22a315d9f218931c8e0f",
+    "url": "/static/js/runtime-main.ba0ea984.js"
+  }
+]);
